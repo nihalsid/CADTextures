4d9d63bd3e2c073fa314058f1492c8534fd6c33d
nihalsid <yawarnihal@gmail.com>
2021-04-26 07:14:06 +0200

    Partial Textures + Fix NOC bug

--- dataset/texture_map_dataset.py
+++ dataset/texture_map_dataset.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from util.misc import read_list
 import numpy as np
 
+from util.timer import Timer
+
 
 class TextureMapDataset(Dataset):
 
@@ -24,12 +26,13 @@ class TextureMapDataset(Dataset):
         if self.preload:
             for index in tqdm(range(len(self.items)), desc='preload_texmap_data'):
                 df, texture, normal, noc, mask_texture = self.load_view_independent_data_from_disk(index)
-                render_list, noc_render_list, mask_render_list = [], [], []
+                render_list, noc_render_list, mask_render_list, partial_texture_list = [], [], [], []
                 for view_index in range(self.views_per_shape):
-                    render, noc_render, mask_render = self.load_view_dependent_data_from_disk(index, view_index)
+                    render, noc_render, mask_render, partial_texture = self.load_view_dependent_data_from_disk(index, view_index)
                     render_list.append(render)
-                    noc_render_list.append(render)
+                    noc_render_list.append(noc_render)
                     mask_render_list.append(mask_render)
+                    partial_texture_list.append(partial_texture)
                 self.preload_dict[self.items[index]] = {
                     'df': df,
                     'texture': texture,
@@ -38,7 +41,8 @@ class TextureMapDataset(Dataset):
                     'noc_render': noc_render_list,
                     'mask_texture': mask_texture,
                     'render': render_list,
-                    'mask_render': mask_render_list
+                    'mask_render': mask_render_list,
+                    'partial_texture': partial_texture_list
                 }
 
         if config.dataset.splits_dir.startswith('overfit'):
@@ -68,17 +72,21 @@ class TextureMapDataset(Dataset):
         image_path = self.path_to_dataset / item / f"rgb_{view_index:03d}.png"
         noc_render_path = self.path_to_dataset / item / f"noc_render_{view_index:03d}.png"
         mask_path = self.path_to_dataset / item / f"silhoutte_{view_index:03d}.png"
+        partial_texture_path = self.path_to_dataset / item / f"partial_texture_{view_index:03d}.png"
         with Image.open(image_path) as render_im:
             render = np.array(render_im).astype(np.float32)
         with Image.open(noc_render_path) as render_noc:
             noc_render = np.array(render_noc).astype(np.float32)
+        with Image.open(partial_texture_path) as partial_tex:
+            partial_texture = np.array(partial_tex).astype(np.float32)
         with Image.open(mask_path) as mask_im:
             mask_render = np.array(mask_im)
             mask_render = np.logical_and(np.logical_and(mask_render[:, :, 0] < 50, mask_render[:, :, 1] < 50), mask_render[:, :, 2] < 50)
         render[~mask_render, :] = 0
         noc_render[~mask_render, :] = 0
         render = render[:, :, :3]
-        return np.ascontiguousarray(np.transpose(render, (2, 0, 1))), np.ascontiguousarray(np.transpose(noc_render, (2, 0, 1))), mask_render[np.newaxis, :, :]
+        noc_render = noc_render[:, :, :3]
+        return np.ascontiguousarray(np.transpose(render, (2, 0, 1))), np.ascontiguousarray(np.transpose(noc_render, (2, 0, 1))), mask_render[np.newaxis, :, :], np.ascontiguousarray(np.transpose(partial_texture, (2, 0, 1)))
 
     def __len__(self):
         return len(self.items)
@@ -88,13 +96,14 @@ class TextureMapDataset(Dataset):
         view_index = random.randint(0, self.views_per_shape - 1)
         if self.preload:
             df, texture, normal, noc, mask_texture = self.preload_dict[item]['df'], self.preload_dict[item]['texture'], self.preload_dict[item]['normal'], self.preload_dict[item]['noc'], self.preload_dict[item]['mask_texture']
-            render_list, noc_render_list, mask_render_list = self.preload_dict[item]['render'], self.preload_dict[item]['noc_render'], self.preload_dict[item]['mask_render']
+            render_list, noc_render_list, mask_render_list, partial_texture_list = self.preload_dict[item]['render'], self.preload_dict[item]['noc_render'], self.preload_dict[item]['mask_render'], self.preload_dict[item]['partial_texture']
             render = render_list[view_index]
             noc_render = noc_render_list[view_index]
             mask_render = mask_render_list[view_index]
+            partial_texture = partial_texture_list[view_index]
         else:
             df, texture, normal, noc, mask_texture = self.load_view_independent_data_from_disk(index)
-            render, noc_render, mask_render = self.load_view_dependent_data_from_disk(index, view_index)
+            render, noc_render, mask_render, partial_texture = self.load_view_dependent_data_from_disk(index, view_index)
         return {
             'name': f'{item}',
             'view_index': view_index,
@@ -105,7 +114,8 @@ class TextureMapDataset(Dataset):
             'noc_render': noc_render,
             'mask_texture': mask_texture.astype(np.float32),
             'render': render,
-            'mask_render': mask_render.astype(np.float32)
+            'mask_render': mask_render.astype(np.float32),
+            'partial_texture': partial_texture
         }
 
     @staticmethod
@@ -115,6 +125,7 @@ class TextureMapDataset(Dataset):
         batch['noc'] = batch['noc'] / 255 - 0.5
         batch['noc_render'] = batch['noc_render'] / 255 - 0.5
         batch['render'] = batch['render'] / 255 - 0.5
+        batch['partial_texture'] = batch['partial_texture'] / 255 - 0.5
 
     @staticmethod
     def move_batch_to_gpu(batch, device):
@@ -125,26 +136,39 @@ class TextureMapDataset(Dataset):
         batch['mask_texture'] = batch['mask_texture'].to(device)
         batch['render'] = batch['render'].to(device)
         batch['mask_render'] = batch['mask_render'].to(device)
+        batch['partial_texture'] = batch['partial_texture'].to(device)
         if type(batch['df']) != list:
             batch['df'] = batch['df'].to(device)
 
     @staticmethod
-    def visualize_sample_pyplot(texture, normal, noc, mask_texture, render, noc_render, mask_render):
+    def visualize_sample_pyplot(texture, normal, noc, mask_texture, render, noc_render, mask_render, partial_texture):
         import matplotlib.pyplot as plt
         texture = np.transpose(texture, (1, 2, 0))
         normal = np.transpose(normal, (1, 2, 0))
         noc = np.transpose(noc, (1, 2, 0))
+        partial_texture = np.transpose(partial_texture, (1, 2, 0))
         render = np.transpose(render, (1, 2, 0))
+        noc_render = np.transpose(noc_render, (1, 2, 0))
         mask_render = mask_render.squeeze()
         mask_texture = mask_texture.squeeze()
-        f, axarr = plt.subplots(2, 4, figsize=(4, 6))
+        f, axarr = plt.subplots(2, 4, figsize=(16, 4))
         axarr[0, 0].imshow(texture + 0.5)
+        axarr[0, 0].axis('off')
         axarr[0, 1].imshow(normal + 0.5)
+        axarr[0, 1].axis('off')
         axarr[0, 2].imshow(render + 0.5)
+        axarr[0, 2].axis('off')
         axarr[0, 3].imshow(noc_render + 0.5)
+        axarr[0, 3].axis('off')
         axarr[1, 0].imshow(noc + 0.5)
+        axarr[1, 0].axis('off')
         axarr[1, 1].imshow(mask_texture + 0.5)
+        axarr[1, 1].axis('off')
         axarr[1, 2].imshow(mask_render + 0.5)
+        axarr[1, 2].axis('off')
+        axarr[1, 3].imshow(partial_texture + 0.5)
+        axarr[1, 3].axis('off')
+        plt.tight_layout()
         plt.show()
 
     @staticmethod
