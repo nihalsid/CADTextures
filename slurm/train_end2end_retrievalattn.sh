e9cfe685381e47d98a7b0fc00d5d4e75894058c5
nihalsid <yawarnihal@gmail.com>
2021-07-16 21:01:18 +0200

    Lower batch size + more gpus + more patches sampled

--- slurm/train_end2end_retrievalattn.sh
+++ slurm/train_end2end_retrievalattn.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name texture_retrfuse
 ##SBATCH --nodes=1
 ##SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=160gb
-#SBATCH --gpus=4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=300gb
+#SBATCH --gpus=8
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=yawar.siddiqui@tum.de
 #SBATCH --exclude=char,pegasus,tarsonis,gondor,moria,seti,sorona,umoja,lothlann
@@ -15,4 +15,4 @@
 
 cd /rhome/ysiddiqui/CADTextures/
 
-python trainer/train_end2end_retrieval_attn.py sanity_steps=1 dataset=single_cube_textures dataset.preload=True experiment=$REXPERIMENT wandb_main=True norm=$RNORM
+python trainer/train_end2end_retrieval_attn.py sanity_steps=1 dataset=single_cube_textures dataset.preload=True experiment=$REXPERIMENT wandb_main=True
